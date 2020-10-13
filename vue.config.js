45ace25b000e08c3cacd82e110cf30d2343f466d
xmoli <fromlili@outlook.com>
2020-10-13 10:46:50 +0800

    add jest. simple service worker

--- vue.config.js
+++ vue.config.js
@@ -14,13 +14,5 @@ module.exports = {
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
         appleMobileWebAppStatusBarStyle: 'black',
-
-        // 配置 workbox 插件
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-        // InjectManifest 模式下 swSrc 是必填的。
-        swSrc: 'src/sw.js',
-        // ...其它 Workbox 选项...
-        }     
     }
 }
