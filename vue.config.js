e97dc71e6d3c5e3dfff375e440d0f2600fdebcdb
xmoli <fromlili@outlook.com>
2020-10-18 17:42:16 +0800

    修改title

--- vue.config.js
+++ vue.config.js
@@ -5,12 +5,12 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = '趣考研 :) 待办事项'
+                args[0].title = ':) 待办事项'
                 return args
             })
     },
     pwa: {
-        name: '趣考研待办事项',
+        name: '待办事项',
         themeColor: '#aeaeae',
         msTileColor: '#000000',
         appleMobileWebAppCapable: 'yes',
